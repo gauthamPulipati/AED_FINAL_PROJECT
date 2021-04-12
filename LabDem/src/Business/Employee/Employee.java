ae97737db0cc3e248e892737c07baa258c5a14f8
gauthamrajsimhapulipati <pulipati.g@gmail.com>
2021-04-11 20:31:24 -0400

    Added uniqueness to users. Final Commit

---
+++ LabDem/src/Business/Employee/Employee.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Employee;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author raunak
+ */
+public class Employee {
+    
+    private String name;
+    private int id;
+    private static int count = 1;
+    ArrayList<Integer> k;
+
+    public Employee() {
+        id = count;
+        count++;
+        k= new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    
+}
