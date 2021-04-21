c255a4cdd437f33b891b444e8602309be6e88bbd
bhosaleakshay666 <bhosale.ak@northeastern.edu>
2021-04-21 18:47:24 -0400

    Added Orders package code. Pending customer attributes

--- AED_FINAL_PROJECT/src/Business/Order/Order.java
+++ AED_FINAL_PROJECT/src/Business/Order/Order.java
@@ -5,10 +5,64 @@
  */
 package Business.Order;
 
+import Business.Products.Product;
+import Business.WorkQueue.WorkRequest;
+
 /**
  *
  * @author aksha
  */
-public class Order {
+public class Order extends WorkRequest {
+  
+    private String orderID;
+    private String orderStatus;
+    //private String DeliveryMan;
+    private double quantity;
+    private Product product;
+    private boolean assign;
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public boolean isAssign() {
+        return assign;
+    }
+
+    public void setAssign(boolean assign) {
+        this.assign = assign;
+    }
     
+   @Override
+    public String toString() {
+        return orderID;
+    } 
 }
--- AED_FINAL_PROJECT/src/Business/Order/OrderDirectory.java
+++ AED_FINAL_PROJECT/src/Business/Order/OrderDirectory.java
@@ -5,10 +5,41 @@
  */
 package Business.Order;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author aksha
  */
 public class OrderDirectory {
+    
+    private ArrayList<Order> orders;
+    
+    public OrderDirectory(){
+        orders = new ArrayList();
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
+    
+    public Order newOrder(){
+        Order order = new Order();
+        orders.add(order);
+        return order;
+    }
+    
+    public Order getOrderID(String id){
+        for (Order order : orders){
+            if(order.getOrderID().equals(id)){
+                return order;
+            }
+        }
+        return null;
+    }
     
 }
